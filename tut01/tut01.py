872406f8c41f17793175c3dc9b365fe63fde4e0f
Chetan Chaudhary <chetanchaudhary015@gmail.com>
2022-08-27 15:35:20 +0530

    factorial of a number

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,6 +1,7 @@
-def factorial(int x):
-    
+def factorial(x):
+    if(x==1):
+        return 1
+    return x*factorial(x-1)
 
-x=int(input("Enter the number whose factorial is to be found"))
-factorial(x)
-"""hello brother"""
+x=int(input("Enter the number whose factorial is to be found\n"))
+print("Factorial of ",x," is ",factorial(x))
