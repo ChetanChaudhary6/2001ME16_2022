e6aec97f68f3ed4bd589b07b9e2a17afd8ff86c5
Chetan Chaudhary <chetanchaudhary015@gmail.com>
2022-09-14 23:38:20 +0530

    taking average and write them

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -10,3 +10,13 @@ with open('octant_input.csv','r') as input_file:
         writer=csv.writer(output_file)
         for row in reader:
             writer.writerow(row)
+data=pd.read_csv('f.csv')
+# taking average using predefine mean method
+u_avg=data['U'].mean()
+v_avg=data['V'].mean()
+w_avg=data['W'].mean()
+# write average on output file
+data.at[0,'U_avg']=u_avg
+data.at[0,'V_avg']=v_avg
+data.at[0,'W_avg']=w_avg
+row_no=0
