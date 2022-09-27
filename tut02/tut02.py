ca8b38e52acccb756122a0be3ef56a6a2a956a95
Chetan Chaudhary <chetanchaudhary015@gmail.com>
2022-09-27 21:39:23 +0530

    making average columns and compute overall id and mod range wise count

--- tut02/tut02.py
+++ tut02/tut02.py
@@ -28,3 +28,122 @@ count_n3=0
 count_p4=0
 count_n4=0
 
+# making average columns 
+for ele in data['V']:
+    x=data.at[row_no,"U'=U-Uavg"]=data.at[row_no,'U']-u_avg
+    y=data.at[row_no,"U'=U-Vavg"]=data.at[row_no,'V']-v_avg
+    z=data.at[row_no,"U'=U-Wavg"]=data.at[row_no,'W']-w_avg
+    if x>0:
+        if y>0:
+            if z>0:
+                data.at[row_no,'Octant']=1
+                count_p1=count_p1+1
+            else:
+                data.at[row_no,'Octant']=-1
+                count_n1=count_n1+1
+        else:
+            if z>0:
+                data.at[row_no,'Octant']=4
+                count_p4=count_p4+1
+            else:
+                data.at[row_no,'Octant']=-4
+                count_n4=count_n4+1
+    else:
+        if y>0:
+            if z>0:
+                data.at[row_no,'Octant']=2
+                count_p2=count_p2+1
+            else:
+                data.at[row_no,'Octant']=-2
+                count_n2=count_n2+1
+        else:
+            if z>0:
+                data.at[row_no,'Octant']=3
+                count_p3=count_p3+1
+            else:
+                data.at[row_no,'Octant']=-3
+                count_n3=count_n3+1
+    row_no=row_no+1
+Octant=data['Octant'].tolist()
+    # count of all octant
+data.at[0,'Octant ID']='Overall Count'
+data.at[0,'1']=count_p1
+data.at[0,'-1']=count_n1
+data.at[0,'2']=count_p2
+data.at[0,'-2']=count_n2
+data.at[0,'3']=count_p3
+data.at[0,'-3']=count_n3
+data.at[0,'4']=count_p4
+data.at[0,'-4']=count_n4
+
+ #now for different mod range values
+count_p1=0
+count_n1=0
+count_p2=0
+count_n2=0
+count_p3=0
+count_n3=0
+count_p4=0
+count_n4=0
+length=row_no
+mod=5000
+j=1
+curr_p1=1
+curr_p2=1
+curr_p3=1
+curr_p4=1
+curr_n1=1
+curr_n2=1
+curr_n3=1
+curr_n4=1
+curr_oct=1
+cnt=0
+
+for i in range(length):
+        x=data.at[i,'Octant']
+        if(x==1): count_p1+=1
+        elif(x==2): count_p2+=1
+        elif(x==3): count_p3+=1
+        elif(x==4): count_p4+=1
+        elif(x==-1): count_n1+=1
+        elif(x==-2): count_n2+=1
+        elif(x==-3): count_n3+=1
+        elif(x==-4): count_n4+=1
+        cnt+=1
+        if(cnt==mod or i==length-1):
+            cnt=0
+            data.at[curr_p1,'1']=count_p1
+            data.at[curr_p2,'2']=count_p2
+            data.at[curr_p3,'3']=count_p3
+            data.at[curr_p4,'4']=count_p4
+            data.at[curr_n1,'-1']=count_n1
+            data.at[curr_n2,'-2']=count_n2
+            data.at[curr_n3,'-3']=count_n3
+            data.at[curr_n4,'-4']=count_n4
+            if(j==6):
+                data.at[curr_oct,'Octant ID']="25001-30000"
+            elif(i==4999):
+                str1="{} - {}".format(i-mod+1, i+1)
+                data.at[curr_oct,'Octant ID']=str1
+            else:
+                str1="{} - {}".format(i-mod+2, i+1)
+                data.at[curr_oct,'Octant ID']=str1
+            curr_p1+=1
+            curr_p2+=1
+            curr_p3+=1
+            curr_p4+=1
+            curr_n1+=1
+            curr_n2+=1
+            curr_n3+=1
+            curr_n4+=1
+            curr_oct+=1
+
+            count_p1=0
+            count_n1=0
+            count_p2=0
+            count_n2=0
+            count_p3=0
+            count_n3=0
+            count_p4=0
+            count_n4=0
+            j+=1
