d7f73805bbe15373d523d2282645da4f19e63ba7
Chetan Chaudhary <chetanchaudhary015@gmail.com>
2022-10-21 11:53:56 +0530

    write and calculate the count of octant in different mod ranges

--- tut05/tut05.py
+++ tut05/tut05.py
@@ -69,3 +69,90 @@ for ele in data['V']:
         print("we can't find valid octant in the given dataframe")
     row_no=row_no+1
 Octant=data['Octant'].tolist()
+    # count of all octant
+data.at[0,'Octant ID']='Overall Count'
+data.at[0,'1']=overallcount[1]=count_p1
+data.at[0,'-1']=overallcount[-1]=count_n1
+data.at[0,'2']=overallcount[2]=count_p2
+data.at[0,'-2']=overallcount[-2]=count_n2
+data.at[0,'3']=overallcount[3]=count_p3
+data.at[0,'-3']=overallcount[-3]=count_n3
+data.at[0,'4']=overallcount[4]=count_p4
+data.at[0,'-4']=overallcount[-4]=count_n4
+overalllist=sorted(overallcount.items(),key=lambda x:x[1],reverse=True)
+ #now for different mod range values
+count_p1=0
+count_n1=0
+count_p2=0
+count_n2=0
+count_p3=0
+count_n3=0
+count_p4=0
+count_n4=0
+length=row_no
+mod=5000
+j=1
+curr_p1=1
+curr_p2=1
+curr_p3=1
+curr_p4=1
+curr_n1=1
+curr_n2=1
+curr_n3=1
+curr_n4=1
+curr_oct=1
+cnt=0
+
+dictmod={}
+grid=[]
+for i in range(length):
+        x=data.at[i,'Octant']
+        if(x==1): count_p1+=1
+        elif(x==2): count_p2+=1
+        elif(x==3): count_p3+=1
+        elif(x==4): count_p4+=1
+        elif(x==-1): count_n1+=1
+        elif(x==-2): count_n2+=1
+        elif(x==-3): count_n3+=1
+        elif(x==-4): count_n4+=1
+        cnt+=1
+        if(cnt==mod or i==length-1):
+            cnt=0
+            data.at[curr_p1,'1']=dictmod[1]=count_p1
+            data.at[curr_p2,'2']=dictmod[2]=count_p2
+            data.at[curr_p3,'3']=dictmod[3]=count_p3
+            data.at[curr_p4,'4']=dictmod[4]=count_p4
+            data.at[curr_n1,'-1']=dictmod[-1]=count_n1
+            data.at[curr_n2,'-2']=dictmod[-2]=count_n2
+            data.at[curr_n3,'-3']=dictmod[-3]=count_n3
+            data.at[curr_n4,'-4']=dictmod[-4]=count_n4
+            list=sorted(dictmod.items(),key=lambda x:x[1],reverse=True)
+            grid.append(list)
+            dictmod={}
+            if(j==6):
+                data.at[curr_oct,'Octant ID']="25001-30000"
+            elif(i==4999):
+                str1="{} - {}".format(i-mod+1, i+1)
+                data.at[curr_oct,'Octant ID']=str1
+            else:
+                str1="{} - {}".format(i-mod+2, i+1)
+                data.at[curr_oct,'Octant ID']=str1
+            curr_p1+=1
+            curr_p2+=1
+            curr_p3+=1
+            curr_p4+=1
+            curr_n1+=1
+            curr_n2+=1
+            curr_n3+=1
+            curr_n4+=1
+            curr_oct+=1
+
+            count_p1=0
+            count_n1=0
+            count_p2=0
+            count_n2=0
+            count_p3=0
+            count_n3=0
+            count_p4=0
+            count_n4=0
+            j+=1
